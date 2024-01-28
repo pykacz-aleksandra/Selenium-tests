925c98dbde2f4c664bd225501fd76310fe074fb6
Ola <pykaczaleksandra@gmail.com>
2024-01-28 17:03:18 +0100

    Adding three sorting tests - correctness of sorting options, sorting by product name (ascending and descending)

--- src/test/java/productSorting.java
+++ src/test/java/productSorting.java
@@ -1,23 +1,27 @@
-import com.google.common.collect.Ordering;
-import org.example.UsersDataSet;
 import org.junit.Assert;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
 
 public class productSorting {
     WebDriver driver;
-    List<WebElement> sortingOptions;
+
+    List<WebElement> actualSortingOptionList = new ArrayList<>();
+
+    WebElement inventoryList;
+    List<String> initialInventoryProductsText = new ArrayList<>();
+    List<String> initialInventoryProductsPriceText = new ArrayList<>();
+    List<String> initialInventoryProductsTextList = new ArrayList<>();
+    Select select;
+
 
     @BeforeEach
     public void initializeChromedriver() {
@@ -27,49 +31,64 @@ public class productSorting {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-    }
 
+        WebElement sortingElement = driver.findElement(By.className("product_sort_container")); //finds webelement with classname "product_sort_container"
+        select = new Select(sortingElement); //create objects of Select class
+        List<WebElement> actualSortingOptionList = select.getOptions();
+        inventoryList = driver.findElement(By.className("inventory_list")); //Find parent element of the products list
+        List<WebElement> initialInventoryProducts = inventoryList.findElements(By.xpath("//div[contains(@class, 'inventory_item_label')]")); //moja poczatkowa lista produktów w defaultowej kolejności
+        initialInventoryProductsText = new ArrayList<>(); //Initialize list of product names that will be sorted
+        for (WebElement product : initialInventoryProducts) { //add product names to the list
+            String productText = product.getText();
+            initialInventoryProductsText.add(productText);
+        }
+        List<WebElement> initialInventoryProductsPriceObjectList = inventoryList.findElements(By.xpath("//div[contains(@class, 'inventory_item_price')]")); //moja poczatkowa lista produktów w defaultowej kolejności
+        initialInventoryProductsPriceText = new ArrayList<>(); //Initialize list of product names that will be sorted
+        for (WebElement product : initialInventoryProductsPriceObjectList) { //add product names to the list
+            String productText = product.getText();
+            initialInventoryProductsPriceText.add(productText);
+        }
+
+    }
     @AfterEach
     public void quitChromedirver() {
         driver.quit();
     }
 
     @Test
-    public void testAreSOrtingOptionsCorrect() {
-        WebElement parentElement = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div/span/select"));
-        sortingOptions = parentElement.findElements(By.xpath(".//*"));
-        List<String> expectedSortingOptions = List.of("Name (A to Z)", "Name (Z to A)", "Price (low to high)", "Price (high to low)");
+    public void testAreSortingOptionsCorrect() {
+        List<String> expectedSortingOptionList = List.of("Name (A to Z)", "Name (Z to A)", "Price (low to high)", "Price (high to low)");
 
-        String option;
-        for (WebElement element : sortingOptions) {
-            option = element.getText();
-            Assert.assertTrue(expectedSortingOptions.contains(option));
+        for (WebElement element : actualSortingOptionList) {
+            Assert.assertTrue(expectedSortingOptionList.contains(element.getText()));
         }
-
     }
     @Test
-    public void correctSorting(){
-        List<String> sortingOptions = List.of("option[value='az']", "option[value='za']", "option[value='lohi']", "option[value='hilo']");
-        driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div/span/select")).click();
+    public void ascendingNameSorting() {
+        Collections.sort(initialInventoryProductsText);
+        select.selectByVisibleText("Name (A to Z)");
 
-        for (String sortingOption : sortingOptions) {
-            driver.findElement(By.cssSelector(sortingOption)).click();
-            List<WebElement> listOfProducts = driver.findElements(By.cssSelector("div.inventory_item_name"));
-            List<String> listOfProductsTitles = new ArrayList<String>();
-            for (WebElement product : listOfProducts) {
-                listOfProductsTitles.add(product.getText());
-            }
-            Assert.assertTrue(Ordering.<String>natural().isOrdered(listOfProductsTitles));
-
-            Assert.assertTrue(Ordering.natural().reverse().isOrdered(listOfProductsTitles));
+        List<WebElement> sortedProducts = inventoryList.findElements(By.xpath("//div[contains(@class, 'inventory_item_label')]"));
+        List<String> sortedProductsTextList = new ArrayList<>();
+        for (WebElement sortedProduct : sortedProducts) {
+            String sortedProductText = sortedProduct.getText();
+            sortedProductsTextList.add(sortedProductText);
         }
-
-
-
-
-
+        Assertions.assertEquals(initialInventoryProductsText, sortedProductsTextList);
+    }
+    @Test
+    public void descendingNameSorting(){
+        Collections.sort(initialInventoryProductsText, Collections.reverseOrder());
+        select.selectByVisibleText("Name (Z to A)");
+        List<WebElement> sortedProducts = inventoryList.findElements(By.xpath("//div[contains(@class, 'inventory_item_label')]"));
+        List<String> sortedProductsTextList = new ArrayList<>();
+        for (WebElement sortedProduct : sortedProducts) {
+            String sortedProductText = sortedProduct.getText();
+            sortedProductsTextList.add(sortedProductText);
         }
+        Assertions.assertEquals(initialInventoryProductsText, sortedProductsTextList);
     }
 
 
+}
 
